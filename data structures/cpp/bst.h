434506b6a81e32a8dc9684fa2297f962d9ad6b09
Angel Lopez Manriquez <sigma271@outlook.com>
2018-06-30 14:49:00 -0500

    Algunos errores con la funcion miembro remove

--- data structures/cpp/bst.h
+++ data structures/cpp/bst.h
@@ -3,7 +3,7 @@
 
 #include <iostream>
 #include <vector>
-#include <new>
+#include <stack>
 
 using namespace std;
 
@@ -32,33 +32,79 @@ private:
         if (root == nullptr) { // no se ha insertado nada previamente
             root = new BinaryNode(data);
         } else {
-            BinaryNode *parent, 
-                *current = root; // al principio estamos en la raiz del arbol
-            BinaryNode *toInsert = new BinaryNode(data);
+            BinaryNode *parent, *current = root; // empezamos estando en la raiz del arbol
+            BinaryNode *toInsert = new BinaryNode(data); 
             bool lastLeft = false; // nos indicara si somos izq. o der. de nuestro padre
             while (current != nullptr) {
                 parent = current;
                 lastLeft = data < current->data;
                 if (data < current->data) current = current->left;
-                else  current = current->right;
+                else if (data > current->data) current = current->right;
+                else return root; // no permitimos duplicados
             }
+
+            // empezamos a conectar el nuevo nodo
             toInsert->parent = parent;
-            if (lastLeft) parent->left = new BinaryNode(data); 
-            else parent->right = new BinaryNode(data);
+            if (lastLeft) parent->left = toInsert; 
+            else parent->right = toInsert;
         }
         return root;
     }
 
     void inorder(BinaryNode *self) {
-        if (self == nullptr) return ;
+        if (self == nullptr) return;
         inorder(self->left);
-        cout << self->data << "\n";
+        cout << self->data << ", ";
         inorder(self->right);
+    }
+
+    BinaryNode *min(BinaryNode *root) {
+        if (root != nullptr)
+            while (root->left != nullptr)
+                root = root->left;
+        return root;
+    }
+
+    void remove(BinaryNode *&self, const T &x) {
+        if (self == nullptr) return; // no encontrado
+        if (x < self->data) remove(self->left, x);
+        else if (self->data < x) remove(self->right, x);
+        else if (self->left != nullptr && self->right != nullptr) { // dos hijos
+            self->data = min(self->right)->data;
+            remove(self->right, x);
+        } else { // el nodo tiene a lo mas 1 hijo
+            BinaryNode *old = self; // guardamos la direccion del nodo a ser borrado
+            self = (self->left != nullptr) ? self->left: self->right;
+            delete old;
+        }
+    }
+
+    inline bool leaf(BinaryNode *node) {
+        return node->right == nullptr && node->left == nullptr;
+    }
+
+    int numberOfNodes(BinaryNode *root) {
+        if (root == nullptr) return 0;
+        return numberOfNodes(root->left) + numberOfNodes(root->right) + 1;
     }
 
 public:
 
     inline bst() { root = nullptr; } 
+
+    bst(vector<T> v) {
+        root = nullptr;
+        for (T x: v) 
+            root = add(root, x);
+    }
+
+    int numberOfNodes() {
+        return numberOfNodes(root);
+    }
+
+    inline void remove(T x) {
+        remove(root, x);
+    }
 
     inline void add(T data) { 
         root = add(root, data); // actualizamos la nueva raiz 
@@ -66,6 +112,28 @@ public:
 
     inline void inorder() { inorder(root); }
 
+    void sort(vector<T> &v) {
+        for (T x: v) add(x);
+
+        // recorrido en inorden
+        stack<BinaryNode *> s;
+        BinaryNode *curr = root;
+        T data;
+        int i = 0;
+        while (true) {
+            if (curr != nullptr) {
+                s.push(curr);
+                curr = curr->left;
+            } else {
+                if (s.empty()) break; // el nodo actual es nulo y la pila esta vacia
+                curr = s.top();  
+                s.pop();
+                data = curr->data;
+                v[i++] = data;
+                curr = curr->right;
+            }
+        }
+    }
 };
 
 #endif 
