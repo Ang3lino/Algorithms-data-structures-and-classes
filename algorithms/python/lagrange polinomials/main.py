7d432fc05e9913081fdd286c74a8d5f48f6d4fb0
Angel Lopez Manriquez <sigma271@outlook.com>
2018-08-16 20:52:34 -0500

    Ya esta :3

--- algorithms/python/lagrange polinomials/main.py
+++ algorithms/python/lagrange polinomials/main.py
@@ -1,24 +1,41 @@
-# pip3 install matplotlib
+""" 
+    La aplicacion corre en python3, en python2 puede fallar. Instalalo con
+        sudo apt install python3
+    Depende de matplotlib, instalala con:
+        pip3 install matplotlib
+    Para correr:
+        python3 main.py LagrangePolynomial.py 
+        python main.py LagrangePolynomial.py (si la primera no funciona)
+    Nota: Basta que en una lista de enteros se encuentre un flotante para que 
+    cada elemento sea del tipo float.
+"""
 
 import matplotlib.pyplot as plt
 import numpy as np
 
 from LagrangePolynomial import *
+from random import * # randint function definition
 
-x = [ 2, 11 / 4, 4 ]
-y = [ 1 / 2, 4 / 11, 1 / 4 ]
-print(x)
+b = int(10 * float(input("ultimo valor del voltaje: ")) +  2)
+x = [ n / 10 for n in range(0, b, 2) ]
+y = list()
+diodetag = input("Nombre del diodo: ")
+#for i in x:
+#    y.append(float(input("v[{}] = ".format(i))))
+y = [ randint(1, 10) / randint(1, 20) for i in range(len(x)) ] # for testing
+
+print(list( zip(x, y) ))
+
 lpol = LagrangePolynomial() 
 lpol.set_points(x, y)
 
-count = 0
-a = []
-b = []
-for xi in x:
-    b.append(lpol.evaluate(xi))
-    a.append(xi)
-    count += 1
-plt.scatter(a, b)
+scale = 1000
+x = [ i / scale  for i in range(int(scale * x[-1])) ]
+y = [ lpol.evaluate(xi) for xi in x ]
 
-print('v:')
+plt.plot(x, y)
+plt.title("Grafica del diodo")
+plt.xlabel("Voltaje (v)")
+plt.ylabel("Corriente (mA)")
+plt.legend([diodetag])
 plt.show()
